6f6f5cdc3d883bf16771b2bcac372c73122302dc
ArthurPatriot <arthur.patriot@gmail.com>
2020-06-03 18:28:08 +0300

    Optimize Camera Move Functions

--- src/Camera.php
+++ src/Camera.php
@@ -5,6 +5,11 @@ namespace PtzCamera;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 
+/**
+ * Class Camera
+ *
+ * @package PtzCamera
+ */
 class Camera
 {
 
@@ -125,114 +130,6 @@ class Camera
 		$data = RequestDataBuilder::ptzDataRelative($posX, $posY, $zoom);
 
 		return $this->makeRequest($url, $data);
-	}
-
-	/**
-	 * Camera PTZ Left
-	 *
-	 * @param int $pan
-	 * @param int $duration
-	 * @return string
-	 */
-	public function left(int $pan = -20, int $duration = 1000)
-	{
-		return $this->momentary($pan, $duration);
-	}
-
-	/**
-	 * Camera PTZ Right
-	 *
-	 * @param int $pan
-	 * @param int $duration
-	 * @return string
-	 */
-	public function right(int $pan = 20, int $duration = 1000)
-	{
-		return $this->momentary($pan, null, null, $duration);
-	}
-
-	/**
-	 * Camera PTZ Up
-	 *
-	 * @param int $tilt
-	 * @param int $duration
-	 * @return string
-	 */
-	public function up(int $tilt = 20, int $duration = 1000)
-	{
-		return $this->momentary(null, $tilt, null, $duration);
-	}
-
-	/**
-	 * Camera PTZ Down
-	 *
-	 * @param int $tilt
-	 * @param int $duration
-	 * @return string
-	 */
-	public function down(int $tilt = -20, int $duration = 1000)
-	{
-		return $this->momentary(null, $tilt, null, $duration);
-	}
-
-	/**
-	 * Camera PTZ Zoom In
-	 *
-	 * @param int $zoom
-	 * @param int $duration
-	 * @return string
-	 */
-	public function zoomIn(int $zoom = 1, int $duration = 1000)
-	{
-		return $this->momentary(null, null, $zoom, $duration);
-	}
-
-	/**
-	 * Camera PTZ Zoom Out
-	 *
-	 * @param int $zoom
-	 * @param int $duration
-	 * @return string
-	 */
-	public function zoomOut(int $zoom = -1, int $duration = 1000)
-	{
-		return $this->momentary(null, null, $zoom, $duration);
-	}
-
-	/**
-	 * Camera PTZ Left / Right Moves
-	 *
-	 * @param int $pan
-	 * @param int $duration
-	 * @return string
-	 */
-	public function pan(int $pan, int $duration = 1000)
-	{
-		return $this->momentary($pan, null, null, $duration);
-	}
-
-	/**
-	 * Camera PTZ Up / Down Moves
-	 *
-	 * @param int $tilt
-	 * @param int $duration
-	 * @return string
-	 */
-	public function tilt(int $tilt, int $duration = 1000)
-	{
-		return $this->momentary(null, $tilt, null, $duration);
-	}
-
-	/**
-	 * Camera PTZ Zoom
-	 *
-	 * @param int $zoom
-	 * @param int $duration
-	 * @return string
-	 */
-	public function zoom(int $zoom, int $duration = 1000)
-	{
-		return $this->momentary(null, null, $zoom, $duration);
 	}
 
 	/**
---
+++ src/CameraMovement.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PtzCamera;
+
+/**
+ * Class CameraMovement
+ *
+ * @package PtzCamera
+ */
+class CameraMovement extends Camera
+{
+
+	/**
+	 * Camera PTZ Left
+	 *
+	 * @param int $pan
+	 * @param int $duration
+	 * @return string
+	 */
+	public function left(int $pan = -20, int $duration = 1000)
+	{
+		return $this->momentary($pan, $duration);
+	}
+
+	/**
+	 * Camera PTZ Right
+	 *
+	 * @param int $pan
+	 * @param int $duration
+	 * @return string
+	 */
+	public function right(int $pan = 20, int $duration = 1000)
+	{
+		return $this->momentary($pan, null, null, $duration);
+	}
+
+	/**
+	 * Camera PTZ Up
+	 *
+	 * @param int $tilt
+	 * @param int $duration
+	 * @return string
+	 */
+	public function up(int $tilt = 20, int $duration = 1000)
+	{
+		return $this->momentary(null, $tilt, null, $duration);
+	}
+
+	/**
+	 * Camera PTZ Down
+	 *
+	 * @param int $tilt
+	 * @param int $duration
+	 * @return string
+	 */
+	public function down(int $tilt = -20, int $duration = 1000)
+	{
+		return $this->momentary(null, $tilt, null, $duration);
+	}
+
+	/**
+	 * Camera PTZ Zoom In
+	 *
+	 * @param int $zoom
+	 * @param int $duration
+	 * @return string
+	 */
+	public function zoomIn(int $zoom = 1, int $duration = 1000)
+	{
+		return $this->momentary(null, null, $zoom, $duration);
+	}
+
+	/**
+	 * Camera PTZ Zoom Out
+	 *
+	 * @param int $zoom
+	 * @param int $duration
+	 * @return string
+	 */
+	public function zoomOut(int $zoom = -1, int $duration = 1000)
+	{
+		return $this->momentary(null, null, $zoom, $duration);
+	}
+
+	/**
+	 * Camera PTZ Left / Right Moves
+	 *
+	 * @param int $pan
+	 * @param int $duration
+	 * @return string
+	 */
+	public function pan(int $pan, int $duration = 1000)
+	{
+		return $this->momentary($pan, null, null, $duration);
+	}
+
+	/**
+	 * Camera PTZ Up / Down Moves
+	 *
+	 * @param int $tilt
+	 * @param int $duration
+	 * @return string
+	 */
+	public function tilt(int $tilt, int $duration = 1000)
+	{
+		return $this->momentary(null, $tilt, null, $duration);
+	}
+
+	/**
+	 * Camera PTZ Zoom
+	 *
+	 * @param int $zoom
+	 * @param int $duration
+	 * @return string
+	 */
+	public function zoom(int $zoom, int $duration = 1000)
+	{
+		return $this->momentary(null, null, $zoom, $duration);
+	}
+
+}
